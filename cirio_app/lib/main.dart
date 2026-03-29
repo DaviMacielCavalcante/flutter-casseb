224738b44ed61704a0248156dfd1aa4f70d4b425
Davi Cavalcante <93160711+DaviMacielCavalcante@users.noreply.github.com>
2026-03-28 21:44:12 -0300

    criando o projeto e adicionando dependências

---
+++ cirio_app/lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MaterialApp(
+    title: "Onde a Santa está?",
+    debugShowCheckedModeBanner: false,
+    home: Scaffold(appBar: AppBar(title: Text("Círio de Nazaré"),),),
+  ));
+}
